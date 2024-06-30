66ee8bfbc0f913338e7b8aa5faa6c9b231249f33
jiunshinn <jiun.dev@gmail.com>
2024-06-30 10:56:47 +0900

    support web

---
+++ packages/app/src/SharedComponent.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const SharedComponent = () => {
+  return (
+    <View>
+      <Text style={{ color: "red" }}>Shared Component</Text>
+    </View>
+  );
+};
+
+export default SharedComponent;
---
+++ packages/web/src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import App from "@my-app/app";
+import SharedComponent from "@my-app/app/src/SharedComponent";
+export default function Home() {
+  return (
+    <main>
+      <App />
+      <SharedComponent />
+    </main>
+  );
+}
